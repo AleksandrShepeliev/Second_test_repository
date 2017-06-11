2ee33b30a22fbd219e1fdbbac557ac08c334f7e1
Yurii Bosov <bosov.yu@gmail.com>
2017-06-11 11:34:03 +0300

    add new file

---
+++ MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  Git_2_lesson
+//
+//  Created by Yurii Bosov on 6/11/17.
+//  Copyright © 2017 Shepeliev Aleksandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
